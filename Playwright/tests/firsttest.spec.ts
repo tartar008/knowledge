5e6eb97e12671b1267af1fd87cb3876e163544fd
LAPTOP-KKBR68BP\ASUS <tartar0085@gmail.com>
2025-10-02 11:19:39 +0700

    เพิ่ม code test ใน firsttest [v1 , v2 , v3]

--- Playwright/tests/firsttest.spec.ts
+++ Playwright/tests/firsttest.spec.ts
@@ -1,6 +1,29 @@
 import { test, expect } from '@playwright/test'
 
 
-test('has title', async ({ page }) => {
-    await page.goto('https://practicetestautomation.com/practice-test-login/')
+test('v-1 login successfully', async ({ page }) => {
+    await page.goto('https://practicetestautomation.com/practice-test-login/');
+    await page.getByLabel('Username').fill('student');
+    await page.getByLabel('Password').fill('Password123');
+    await page.getByRole('button', { name: 'Submit' }).click();
+    expect(page.url()).toBe('https://practicetestautomation.com/logged-in-successfully/')
+    expect(page.getByRole('heading', { name: 'Logged In Successfully' }))
+});
+
+test('v-2 login fail (username ผิด)', async ({ page }) => {
+    await page.goto('https://practicetestautomation.com/practice-test-login/');
+    await page.getByLabel('Username').fill('incorrectUser');
+    await page.getByLabel('Password').fill('Password123');
+    await page.getByRole('button', { name: 'Submit' }).click();
+    expect(page.url()).toBe('https://practicetestautomation.com/practice-test-login/')
+    expect(page.getByRole('heading', { name: 'Your username is invalid!' }))
+});
+
+test('v-3 login fail (password ผิด)', async ({ page }) => {
+    await page.goto('https://practicetestautomation.com/practice-test-login/');
+    await page.getByLabel('Username').fill('student');
+    await page.getByLabel('Password').fill('incorrectPassword ');
+    await page.getByRole('button', { name: 'Submit' }).click();
+    expect(page.url()).toBe('https://practicetestautomation.com/practice-test-login/')
+    expect(page.getByRole('heading', { name: 'Your password is invalid!' }))
 })
